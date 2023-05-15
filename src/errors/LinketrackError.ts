388cb508893106812f4aa3b72db566699b7dc0c9
diaszano <luucas.dsantos@gmail.com>
2023-05-14 22:13:36 -0300

    feature/createReadme: Documentado o LinketrackError.ts

--- src/errors/LinketrackError.ts
+++ src/errors/LinketrackError.ts
@@ -1,4 +1,11 @@
+/**
+ * Classe de erro personalizada para erros relacionados à linketrackjs.
+ */
 export default class LinketrackError extends Error {
+  /**
+   * Construtor da classe LinketrackError.
+   * @param {string} message - Mensagem de erro.
+   */
   constructor(message: string) {
     super(message);
     this.name = 'LinketrackError';
