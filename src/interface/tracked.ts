4aaf60ec49cf1932c9372e3554afc424e14292a8
diaszano <luucas.dsantos@gmail.com>
2023-12-04 22:41:08 -0300

    feat: Criadas interfaces de retorno de dados.

---
+++ src/interface/tracked.ts
@@ -0,0 +1,53 @@
+export interface Tracked {
+  /**
+   * Código de rastreio da encomenda.
+   */
+  code: string;
+
+  /**
+   * Serviço de entrega da encomenda.
+   */
+  service: string;
+
+  /**
+   * Host de rastreio da encomenda.
+   */
+  host: string;
+
+  /**
+   * Quantidade de eventos que contêm a encomenda.
+   */
+  quantity: number;
+
+  /**
+   * Lista de eventos associados à encomenda.
+   */
+  events: Event[];
+
+  /**
+   * Carimbo de tempo do pedido.
+   */
+  time: number;
+
+  /**
+   * Data do último evento.
+   */
+  lastEvent: Date;
+}
+
+export interface Event {
+  /**
+   * Data em que o evento foi criado.
+   */
+  date: Date;
+
+  /**
+   * Hora em que o evento foi criado.
+   */
+  location: string;
+
+  /**
+   * Local em que o evento foi criado.
+   */
+  status: string;
+}
